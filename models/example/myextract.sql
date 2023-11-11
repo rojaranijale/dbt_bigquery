95d1a2270ef2c9bb52da3d75652bf6207a9691ae
Roja Rani Jale <rojasridhara@gmail.com>
2023-11-11 18:53:53 +0000

    extract3

--- models/example/myextract.sql
+++ models/example/myextract.sql
@@ -2,16 +2,9 @@
 
 WITH extracted_numbers AS (
   SELECT
-    Hours_of_Videos_Uploaded_on_YouTube_Every_Day,
+    "Hours of Videos Uploaded on YouTube Every Day",
     -- Extract the numeric value using REGEXP_EXTRACT
-    CAST(REGEXP_EXTRACT(Hours_of_Videos_Uploaded_on_YouTube_Every_Day, r'(\d+) hours?') AS INT64) AS extracted_number
+    CAST(REGEXP_EXTRACT("Hours of Videos Uploaded on YouTube Every Day", r'(\d+) hours?') AS INT64) AS extracted_number
   FROM
-    {{ ref(`de2roja.youtube2.st1_Videos upload`) }}
+    {{ ref('de2roja.youtube2.st1_Videos upload') }}
 )
-
--- You can then use the extracted numbers in further transformations or analysis
-SELECT
-  
-  extracted_number
-FROM
-  extracted_numbers;
