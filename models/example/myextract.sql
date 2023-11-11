55dd239d588007ff7b76b5aa7db9d3c29f81438a
Roja Rani Jale <rojasridhara@gmail.com>
2023-11-11 19:36:11 +0000

    successextract

--- models/example/myextract.sql
+++ models/example/myextract.sql
@@ -1,10 +1,13 @@
--- models/your_model.sql
-
 WITH extracted_numbers AS (
-  SELECT
-    "Hours of Videos Uploaded on YouTube Every Day",
-    -- Extract the numeric value using REGEXP_EXTRACT
-    CAST(REGEXP_EXTRACT("Hours of Videos Uploaded on YouTube Every Day", r'(\d+) hours?') AS INT64) AS extracted_number
+  SELECT 
+    Hours_of_Videos_Uploaded_on_YouTube_Every_Day,
+    -- Extract the numeric value until the first space
+    CAST(SPLIT(REGEXP_EXTRACT(Hours_of_Videos_Uploaded_on_YouTube_Every_Day, r'(\d+)\s*'), ' ')[OFFSET(0)] AS INT64) AS extracted_number
   FROM
-    {{ ref('de2roja.youtube2.st1_Videos upload') }}
+    de2roja.youtube2.st1_Videos_upload_copy
 )
+
+SELECT
+  Hours_of_Videos_Uploaded_on_YouTube_Every_Day,
+  extracted_number
+FROM extracted_numbers
